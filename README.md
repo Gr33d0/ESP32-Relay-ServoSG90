c5f7fba0c05227ae8c91746fc52e299027509a43
Gr33d0 <80392598+Gr33d0@users.noreply.github.com>
2025-05-27 19:03:36 +0000

    Funcional

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # ESP32-Relay-ServoSG90
 ESP32 activate Relay that activate servo
+
+List of items:
+
+Esp32-TTGO
+Servo SG90
+Relay 5v
+External battery
+
+Board: LilyGo T-Display
---
+++ code.ino
@@ -0,0 +1,40 @@
+#include <ESP32Servo.h>
+
+#define RELAY_PIN 16
+
+Servo meuServo;
+void setup() {
+  pinMode(RELAY_PIN, OUTPUT);
+  digitalWrite(RELAY_PIN, LOW); // Relé desligado no início
+  meuServo.attach(17);
+  Serial.begin(115200);
+  
+}
+
+void loop() {
+
+
+
+  // Liga o relé
+  digitalWrite(RELAY_PIN, HIGH);
+
+
+  // Verifica o estado atual
+  int estado = digitalRead(RELAY_PIN);
+  Serial.print("Estado do relé: ");
+  Serial.println(estado == HIGH ? "LIGADO" : "DESLIGADO");
+  meuServo.write(0);    // Vai para 0 graus
+  delay(1000);
+  meuServo.write(90);   // Vai para 90 graus
+  delay(1000);
+  meuServo.write(180);  // Vai para 180 graus
+  delay(1000);
+  // Desliga o relé
+  digitalWrite(RELAY_PIN, LOW);
+ delay(1000);
+
+  // Verifica novamente
+  estado = digitalRead(RELAY_PIN);
+  Serial.print("Estado do relé: ");
+  Serial.println(estado == HIGH ? "LIGADO" : "DESLIGADO");
+}
